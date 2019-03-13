046a2c90a81170bd5c26314982806b59f2947ebd
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-13 17:14:03 +0000

    Automatically backed up by Learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -8,11 +8,11 @@ class FilteredFruitList extends Component {
 
 
   render() {
-    const list = !this.props.filter || this.props.filter === 'all' ? this.props.items : this.props.items.filter(i => i.fruit_type === this.props.filter);
+    //const list = !this.props.filter || this.props.filter === 'all' ? this.props.items : this.props.items.filter(i => i.fruit_type === this.props.filter);
 
     return (
       <ul className="fruit-list">
-        {list.map((item, index) => <li key={index}>{item.char}</li>)}
+        {this.props.list.map((item, index) => <li key={index}>{item.char}</li>)}
       </ul>
     );
   }
