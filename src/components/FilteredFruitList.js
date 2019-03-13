e450155efef2e196ea393d672646cbd6e9f2d1e9
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-13 17:52:17 +0000

    Automatically backed up by Learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
-    const FilteredFruitList = props =>
-      <ul className="fruit-list">
-        {props.list.map((item, index) => <li key={index}>{item.char}</li>)}
-      </ul>;
+const FilteredFruitList = props =>
+  <ul className="fruit-list">
+    {props.list.map((item, index) => <li key={index}>{item.char}</li>)}
+  </ul>;
 
-
+FilteredFruitList.defaultProps = {
+    list: []
+}
 export default FilteredFruitList;
