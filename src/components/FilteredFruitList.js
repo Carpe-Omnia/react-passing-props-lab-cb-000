b120fe84669fcb0a6067c6a1a7b439f1b59c89d9
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-13 17:25:32 +0000

    Automatically backed up by Learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -12,7 +12,7 @@ import React, { Component } from 'react';
 
   //  return (
     const FilteredFruitList = props =>
-      const list = !this.props.filter || this.props.filter === 'all' ? this.props.items : this.props.items.filter(i => i.fruit_type === this.props.filter);
+      const list = !props.filter || props.filter === 'all' ? props.items : props.items.filter(i => i.fruit_type === props.filter);
       <ul className="fruit-list">
         {list.map((item, index) => <li key={index}>{item.char}</li>)}
       </ul>;
