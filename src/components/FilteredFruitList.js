6fad441d361ade4d371522520051ce222a6d7cd3
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-13 17:52:42 +0000

    Automatically backed up by Learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -4,8 +4,9 @@ const FilteredFruitList = props =>
   <ul className="fruit-list">
     {props.list.map((item, index) => <li key={index}>{item.char}</li>)}
   </ul>;
-
+/*
 FilteredFruitList.defaultProps = {
     list: []
 }
+*/
 export default FilteredFruitList;
