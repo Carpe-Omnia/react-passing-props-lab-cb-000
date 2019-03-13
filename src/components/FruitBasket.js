0fce4d3c5b4e9095ed1f5b488282f09a2e2f4bb7
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-13 17:13:46 +0000

    Automatically backed up by Learn

--- src/components/FruitBasket.js
+++ src/components/FruitBasket.js
@@ -6,7 +6,7 @@ import FilteredFruitList from './FilteredFruitList.js';
 const FruitBasket = props =>
       <div className="fruit-basket">
         <Filter handleChange={props.handleFilterChange} filters={props.filters} />
-        <FilteredFruitList filter={props.selectedFilter} items={props.items} />
+        <FilteredFruitList filter={props.selectedFilter} items={props.items} list={props.list} />
       </div>;
 
 export default FruitBasket;
